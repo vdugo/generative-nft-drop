655fb2b4342dc30d0e46cdd2523e4ca2345dc749
Vincent <v3rdugo@yahoo.com>
2023-06-22 15:45:28 -0700

    setting up NFT contract and deploying

---
+++ test/NFT.js
@@ -0,0 +1,70 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => 
+{
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const ether = tokens
+
+describe('NFT', () => 
+{
+    const NAME = 'Dapp Punks'
+    const SYMBOL = 'DP'
+    const COST = ether(10)
+    const MAX_SUPPLY = 25
+    const BASE_URI = 'ipfs://QmQ2jnDYecFhrf3asEWjyjZRX1pZSsNWG3qHzmNDvXa9qg/'
+    let nft, accounts, owner, minter
+
+    beforeEach(async () =>
+    {
+        accounts = await ethers.getSigners()
+        owner = accounts[0]
+        minter = accounts[1]
+    })
+
+    describe('Deployment', () => 
+    {
+        // 2 minutes from now
+        const ALLOW_MINTING_ON = (Date.now() + 120000).toString().slice(0, 10)
+
+        beforeEach(async () => 
+        {
+          const NFT = await ethers.getContractFactory('NFT')
+          nft = await NFT.deploy(NAME, SYMBOL, COST, MAX_SUPPLY, ALLOW_MINTING_ON, BASE_URI)
+        })
+    
+        it('has correct name', async () => 
+        {
+          expect(await nft.name()).to.equal(NAME)
+        })
+
+        it('has correct symbol', async () => 
+        {
+          expect(await nft.symbol()).to.equal(SYMBOL)
+        })       
+        it('it returns the cost to mint', async () =>
+        {
+            expect(await nft.cost()).to.equal(COST)
+        })      
+        it('returns the maximum total supply', async () =>
+        {
+          expect(await nft.maxSupply()).to.equal(MAX_SUPPLY)
+        })
+        it('returns the allowed minting time', async () =>
+        {
+          expect(await nft.allowMintingOn()).to.equal(ALLOW_MINTING_ON)
+        })
+        it('returns the base URI', async () =>
+        {
+          expect(await nft.baseURI()).to.equal(BASE_URI)
+        })
+        it('returns the owner', async () =>
+        {
+          expect(await nft.owner()).to.equal(owner.address)
+        })
+
+    })
+
+})
